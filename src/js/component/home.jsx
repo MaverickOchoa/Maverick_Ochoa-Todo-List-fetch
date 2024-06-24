f76c1fe0061e4dd6d497ca402d0170148d40f584
MaverickOchoa <126739291+MaverickOchoa@users.noreply.github.com>
2024-06-24 23:33:34 +0000

    finished todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const firstWork ={
+	"list" : ""
+}
+
 
 //create your first component
 const Home = () => {
+
+	const [todo, setTodo] = useState(firstWork)
+	const [error, setError] = useState(false)
+	const [todoList, setTodoList] = useState([
+		
+	])
+
+
+	const handleChange = (event) => {
+		setTodo({
+			...todo,
+			[event.target.name]: event.target.value,
+		})
+	}
+
+	const handleSubmit = (event) =>{
+		event.preventDefault()
+
+		//validar si los campos estan llenos
+		if (todo.list.trim() === ""){
+			setError(true)
+			return
+		}
+
+		setTodoList([...todoList, todo])
+		setTodo(firstWork)
+		setError(false)
+	}
+
+	const handleDelete = (index) => {
+		const newList = todoList.filter((_, i) => i !== index)
+		setTodoList(newList)
+
+
+			
+
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center justify-content-center d-flex">
+			<div className="col-12 bg px-5">
+				<div className="col-12 mt-5">
+					<h1 className="title">To-do's</h1>
+				</div>
+
+				<div className="col-12 form-group">
+					<form action="" onSubmit={handleSubmit}>
+					<input type="text" 
+						className="form-control " 
+						id="txtList"
+						name="list"
+						value={todo.list}
+						placeholder="What needs to be done?"
+						onChange={handleChange}
+
+					/>
+					<button type="submit" className="btn btn-primary mt-5 col-12"> Añadir Actividad</button>
+					</form>
+
+					<div className="col-12">
+						{error ? <h5>Ingresa una actividad</h5> : null }
+					</div>
+
+					<div className="col-12">
+						<ul>
+							{
+								todoList.map((item,index) =>{
+									return (
+										<li key={index}>
+											<p>{item.list}</p>
+											
+											<button className="btn btn-primary" onClick={() => handleDelete(index)}>Done</button>
+										</li>
+									)
+								})
+							}
+						</ul>
+					</div>
+					
+				</div>
+			</div>
 		</div>
 	);
 };
